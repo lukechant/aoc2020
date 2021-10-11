2f4b785133b33a211a62f79406e7826a994f97d9
luke-chant <luke.chant@autotrader.co.uk>
2021-10-11 16:18:47 +0100

    refactor

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -3,48 +3,53 @@ import { test, readInput } from "../utils/index"
 const prepareInput = (rawInput: string) => rawInput
 
 const input = prepareInput(readInput())
+  .split("\n")
+  .map((input) => Number(input))
+
+let firstNumber: number
+let secondNumber: number
+let thirdNumber: number
 
 const goA = (input) => {
-  const arrayOfInputs = input.split("\n");
-  const arrayOfNumbers = arrayOfInputs.map(input => Number(input));
-  let firstNumber:number;
-  let secondNumber:number;
-  for(let indexA = 0; indexA < arrayOfNumbers.length; indexA++){
-    for(let indexB = indexA + 1; indexB < arrayOfNumbers.length; indexB++){
-       if(arrayOfNumbers[indexA] + arrayOfNumbers[indexB] == 2020){
-        firstNumber = arrayOfNumbers[indexA];
-        secondNumber = arrayOfNumbers[indexB];
-       }
+  for (let indexA = 0; indexA < input.length; indexA++) {
+    for (let indexB = indexA + 1; indexB < input.length; indexB++) {
+      if (input[indexA] + input[indexB] == 2020) {
+        firstNumber = input[indexA]
+        secondNumber = input[indexB]
+      }
     }
   }
-  console.log(firstNumber);
-  console.log(secondNumber);
+  console.log(firstNumber)
+  console.log(secondNumber)
   return firstNumber * secondNumber;
 }
 
 const goB = (input) => {
-  const arrayOfInputs = input.split("\n");
-  const arrayOfNumbers = arrayOfInputs.map(input => Number(input));
-  let firstNumber:number;
-  let secondNumber:number;
-  let thirdNumber:number;
-  for(let indexA = 0; indexA < arrayOfNumbers.length; indexA++){
-    for(let indexB = indexA + 1; indexB < arrayOfNumbers.length; indexB++){
-      for(let indexC = indexB + 1; indexC < arrayOfNumbers.length; indexC++){
-       if(arrayOfNumbers[indexA] + arrayOfNumbers[indexB] + arrayOfNumbers[indexC] == 2020){
-        firstNumber = arrayOfNumbers[indexA];
-        secondNumber = arrayOfNumbers[indexB];
-        thirdNumber = arrayOfNumbers[indexC];
-       }
+  for (let indexA = 0; indexA < input.length; indexA++) {
+    for (let indexB = indexA + 1; indexB < input.length; indexB++) {
+      for (let indexC = indexB + 1; indexC < input.length; indexC++) {
+        if (input[indexA] + input[indexB] + input[indexC] == 2020) {
+          firstNumber = input[indexA]
+          secondNumber = input[indexB]
+          thirdNumber = input[indexC]
+        }
+      }
     }
   }
-  }
-  console.log(firstNumber);
-  console.log(secondNumber);
-  console.log(thirdNumber);
-  return firstNumber * secondNumber * thirdNumber;
+  console.log(firstNumber)
+  console.log(secondNumber)
+  console.log(thirdNumber)
+  return firstNumber * secondNumber * thirdNumber
 }
 
+// const map1 = input.map((element, index) => element[index] + element[index++] );
+// const testA = input.forEach(input.reduce((prev, curr) => { if(prev + curr === 2020) {
+//   return prev*curr; }}));
+// const test3 = input.forEach((element) => {
+//   return element * 2;
+// });
+
+// console.log(test3);
 /* Tests */
 
 // test()
@@ -58,3 +63,4 @@ console.timeEnd("Time")
 
 console.log("Solution to part 1:", resultA)
 console.log("Solution to part 2:", resultB)
+// console.log(testA)
