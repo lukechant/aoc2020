ee3705f01a7a310bae59b51845c7e11c523b6096
luke-chant <luke.chant@autotrader.co.uk>
2021-11-15 15:55:12 +0000

    add solution for day 3

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -4,24 +4,35 @@ const prepareInput = (rawInput: string) => rawInput
 
 const input = prepareInput(readInput()).split('\n');
 
-let row = 0;
-let trees = 0;
-
-const goA = (input) => {
-  for(let column = 0; column < input.length; column++) {
-    row += 3;
-    if (row > input[column].length) {
-      row = input[column].length - 3;
+const treeLooper = (input, columnIncrement, rowIncrement) => {
+  let trees = 0;
+  let column = 0;
+  let row = 0 + rowIncrement;
+  
+  for(row; row < input.length; row += rowIncrement) {
+    column += columnIncrement;
+    if (column > input[row].length -1 ) {
+      column = column - input[row].length
     }
-    if (input[column].charAt(row)=='#') {
+
+    if (input[row].charAt([column])=='#') {
       trees++;
     }
   }
   return trees;
 }
 
+const goA = (input) => {
+  return treeLooper(input, 3, 1)
+}
+
 const goB = (input) => {
-  return
+  let slopeA = treeLooper(input, 1, 1)
+  let slopeB = treeLooper(input, 3, 1)
+  let slopeC = treeLooper(input, 5, 1)
+  let slopeD = treeLooper(input, 7, 1)
+  let slopeE = treeLooper(input, 1, 2)
+  return slopeA * slopeB * slopeC * slopeD * slopeE
 }
 
 /* Tests */
